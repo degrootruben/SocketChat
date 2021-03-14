ceb15dddfdfcbc022f633b1483dd10529b761dbd
Ruben de Groot <rpg.degroot@gmail.com>
2021-03-14 21:08:51 +0100

    Edited package.json

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const PORT = process.env.PORT | 8000;
 const app = express();
@@ -10,7 +11,10 @@ const io = require("socket.io")(httpServer, {
 });
 
 if (process.env.NODE_ENV === "production") {
-    
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
 }
 
 io.on("connection", socket => {  
