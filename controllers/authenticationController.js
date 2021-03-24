4a6743b93ba0a00bad85436145294e3ac4f0fff1
Ruben de Groot <rpg.degroot@gmail.com>
2021-03-24 16:14:16 +0100

    Recreated database and removed credentials from code

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -1,6 +1,4 @@
-const { Pool, Client } = require("pg");
-
-
+const { Client } = require("pg");
 
 module.exports.loginUser = async (req, res) => {
     if (req.body.username && req.body.password) {
@@ -11,18 +9,7 @@ module.exports.loginUser = async (req, res) => {
 }
 
 module.exports.registerUser = async (req, res) => {
-    const pool = new Pool({
-        user: "spzkggifjzyakm",
-        password: "a0f738e98f7070a92bf98aefa495e1498a927e9edfd277cc15b561db834cb54b",
-        host: "ec2-52-21-252-142.compute-1.amazonaws.com",
-        port: 5432,
-        database: "d4f74sdqddblfg"
-    });
-
-    pool.query("SELECT NOW()", (err, res) => {
-        console.log(err, res);
-        pool.end();
-    });
+    
 
     res.end();
 }
